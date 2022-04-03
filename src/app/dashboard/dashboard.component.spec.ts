0820c3a428df2e8d97b12afe3c35e55431f5b9a0
sreenadh319 <69348802+sreenadh319@users.noreply.github.com>
2022-04-03 14:59:09 +0100

    my example

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -22,4 +23,13 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  fit('should set banner background',()=>{
+    fixture.detectChanges();
+    const ele = fixture.debugElement.query(By.css('.dashboard')).nativeElement;
+    console.log(ele.style.backgroundImage);
+    console.log(getComputedStyle(ele).backgroundImage);
+  });
+
+
 });
